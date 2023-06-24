bf2f46d21b52f666a3b68d5cef7fabd58134f557
SBSun <46879319+SBSun@users.noreply.github.com>
2023-06-24 12:57:03 +0900

    Create Project

---
+++ src/main/java/com/jamanchi/JamanchiApplication.java
@@ -0,0 +1,13 @@
+package com.jamanchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamanchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamanchiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jamanchi/jamanchi/JamanchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamanchi.jamanchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamanchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
