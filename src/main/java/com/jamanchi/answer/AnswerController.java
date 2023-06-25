c2632958f1753f63a1f16a42fa4be601bd1e2bf1
SBSun <46879319+SBSun@users.noreply.github.com>
2023-06-26 03:11:52 +0900

    Feat : Answer API에 CORS 추가

--- src/main/java/com/jamanchi/answer/AnswerController.java
+++ src/main/java/com/jamanchi/answer/AnswerController.java
@@ -5,18 +5,16 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "답변")
 @RestController
 @RequestMapping("/api/v1/answer")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "https://port-0-jamanchi-back-7xwyjq992llj9hm2l9.sel4.cloudtype.app/", maxAge = 3600)
 public class AnswerController {
     private final AnswerService answerService;
-
+    
     @GetMapping("")
     @Operation(summary = "답변 결과 조회", description = "취미정보를 토대로 db 데이터에 있는 답변을 받아옵니다. (가격, 필요물품, 소개 기본제공으로 추가 키워드id만 전달필요)")
     public ResponseEntity<AnswerResponseDto.DbResponse> getAnswerDB(@RequestParam Integer hobbyId, @RequestParam Integer keywordId1, @RequestParam Integer keywordId2) {
