95739640e18c8a183a5b52707896487581132b4c
SBSun <46879319+SBSun@users.noreply.github.com>
2023-06-26 03:47:40 +0900

    Feat : WebConfig 구현

--- src/main/java/com/jamanchi/answer/AnswerController.java
+++ src/main/java/com/jamanchi/answer/AnswerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/answer")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", maxAge = 3600, methods = RequestMethod.GET)
 public class AnswerController {
     private final AnswerService answerService;
 
