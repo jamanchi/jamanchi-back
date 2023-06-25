4275c3fe7555f3151c9e192d3fe98bd1edff46ce
maltepoo <78013903+maltepoo@users.noreply.github.com>
2023-06-26 04:15:45 +0900

    Update AnswerController.java

--- src/main/java/com/jamanchi/answer/AnswerController.java
+++ src/main/java/com/jamanchi/answer/AnswerController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/answer")
 @RequiredArgsConstructor
-@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class AnswerController {
     private final AnswerService answerService;
 
